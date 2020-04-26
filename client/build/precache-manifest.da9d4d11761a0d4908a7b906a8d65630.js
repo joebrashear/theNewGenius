8a350050e8d32cc4cbd09ebfa695d5c5e9f0ecc0
Joe Brashear <joebrashear@live.com>
2020-04-26 09:13:01 -0400

    Hopefully this works

---
+++ client/build/precache-manifest.da9d4d11761a0d4908a7b906a8d65630.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd012517c104dfc8207f783e397271c9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ff222045f2b9e0e12447",
+    "url": "/static/css/main.918a7009.chunk.css"
+  },
+  {
+    "revision": "65a96d3db8e6010cb741",
+    "url": "/static/js/2.490a98ba.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.490a98ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff222045f2b9e0e12447",
+    "url": "/static/js/main.94b2b60b.chunk.js"
+  },
+  {
+    "revision": "702554416a81a1e3a894",
+    "url": "/static/js/runtime-main.784c586e.js"
+  }
+]);
